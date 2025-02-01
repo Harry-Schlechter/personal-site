8f7059e44a6e81c317bc9401e8d447bfe9ae8f75
Harry Schlechter <99295520+Harry-Schlechter@users.noreply.github.com>
2025-02-01 16:29:29 -0500

    build fix

--- my-app/src/pages/typed.tsx
+++ my-app/src/pages/typed.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
 
 const TypewriterEffect: React.FC = () => {
   const [text, setText] = useState(''); // State for the current text
